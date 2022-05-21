f51693a7fbc67d58fefeeaae96a1a82f9e929e04
Chris Ohk <utilforever@gmail.com>
2022-05-22 00:53:56 +0900

    feat: Add simple test code for checking a method 'next_token()'

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,7 +1,42 @@
+use crate::token::token::Token;
+
 pub struct Lexer {}
 
 impl Lexer {
     pub fn new() -> Self {
         Self {}
+    }
+
+    pub fn next_token(&self) -> Token {
+        Token::Illegal
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::lexer::lexer::Lexer;
+    use crate::token::token::Token;
+
+    #[test]
+    fn test_next_token() {
+        let input = r#"=+(){},;"#;
+        let tests = vec![
+            Token::Assign,
+            Token::Plus,
+            Token::Lparen,
+            Token::Rparen,
+            Token::Lbrace,
+            Token::Rbrace,
+            Token::Comma,
+            Token::Semicolon,
+            Token::Eof,
+        ];
+
+        let mut lexer = Lexer::new();
+
+        for expect in tests {
+            let tok = lexer.next_token();
+            assert_eq!(expect, tok);
+        }
     }
 }
--- src/token/token.rs
+++ src/token/token.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, PartialEq)]
 pub enum Token {
     Illegal,
     Eof,
