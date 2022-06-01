d49dca038594e4bed8a63a00493fc0a2c37edb9e
Chris Ohk <utilforever@gmail.com>
2022-06-01 20:56:19 +0900

    test: Extend test case to support new tokens

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -58,15 +58,49 @@ mod tests {
 
     #[test]
     fn test_next_token() {
-        let input = r#"=+(){},;"#;
+        let input = r#"let five = 5;
+let ten = 10;
+
+let add = fn(x, y) {
+    x + y;
+};
+"#;
         let tests = vec![
+            Token::Let,
+            Token::Ident(String::from("five")),
             Token::Assign,
-            Token::Plus,
+            Token::Int(5),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("ten")),
+            Token::Assign,
+            Token::Int(10),
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("add")),
+            Token::Assign,
+            Token::Func,
             Token::Lparen,
+            Token::Ident(String::from("x")),
+            Token::Comma,
+            Token::Ident(String::from("y")),
             Token::Rparen,
             Token::Lbrace,
+            Token::Ident(String::from("x")),
+            Token::Plus,
+            Token::Ident(String::from("y")),
+            Token::Semicolon,
             Token::Rbrace,
+            Token::Semicolon,
+            Token::Let,
+            Token::Ident(String::from("result")),
+            Token::Assign,
+            Token::Ident(String::from("add")),
+            Token::Lparen,
+            Token::Ident(String::from("five")),
             Token::Comma,
+            Token::Ident(String::from("ten")),
+            Token::Rparen,
             Token::Semicolon,
             Token::Eof,
         ];
