3346426b75ba19b8c713ed187cb71beef544643c
Chris Ohk <utilforever@gmail.com>
2022-07-10 13:15:58 +0900

    feat: Implement method 'new()'

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,7 +1,23 @@
-pub struct Parser {}
+use crate::lexer::lexer::Lexer;
+use crate::token::token::Token;
 
-impl Parser {
-    pub fn new() -> Self {
-        Self {}
+pub struct Parser<'a> {
+    lexer: Lexer<'a>,
+    cur_token: Token,
+    peek_token: Token,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(lexer: Lexer<'a>) -> Self {
+        let mut parser = Parser {
+            lexer,
+            cur_token: Token::Eof,
+            peek_token: Token::Eof,
+        };
+
+        // parser.next_token();
+        // parser.next_token();
+
+        parser
     }
 }
